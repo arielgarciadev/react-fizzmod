fe288ce6ca06f7dc645f7d634ecaf1e63175c514
Ariel Garcia <ga.arielgarcia@gmail.com>
2019-08-01 21:54:39 -0300

    Clase 4 - Ajax

--- ajax.js
+++ ajax.js
@@ -1,8 +1,14 @@
 let xhr = new XMLHttpRequest;
 
 xhr.addEventListener("readystatechange", () => {
-    console.log(`Nuevo estado ${xhr.readyState}`)
-} )
+    console.log(`Nuevo estado ${xhr.readyState}`);  
+})
+
+xhr.addEventListener("load", () => {
+    if(xhr.status == 200){
+        console.log(xhr.response);
+    }    
+})
 
 xhr.open("GET", "info.txt");
 
